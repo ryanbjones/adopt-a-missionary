5a9495aa8e70e9691b337dcd6e9a389be14b42a5
Ryan Jones <run0391@gmail.com>
2016-09-14 23:15:06 -0600

    model notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+MISSIONARY
+name
+mission_id
+birthday
+country
+state
+mission term
+photo?
+
+MODERATOR
+name
+mission_id
+email_address
+username
+password
+
+MISSION
+id
+name
+address
+mission_president_name
+
+SUGGESTED_GIFT
+id
+name
+description
+missionary_id
+
+ADOPTOR
+username
+password
+first_name
+last_name
+email_address
+photo
+
+ADOPTION
+missionary_id
+adoptor_id
